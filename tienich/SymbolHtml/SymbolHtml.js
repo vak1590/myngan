2e8c56dfe3b479d8efe65126842995d5b7d16184
vak1590 <53036545+vak1590@users.noreply.github.com>
2022-05-19 13:53:50 +0700

    Update SymbolHtml.js

--- tienich/SymbolHtml/SymbolHtml.js
+++ tienich/SymbolHtml/SymbolHtml.js
@@ -1,23 +1,23 @@
 
-    $('.boxicons span').click(function () {
-        let e = $(this);
+    jQuery('.boxicons span').click(function () {
+        let e = jQuery(this);
         let entity = e.attr('data-entity');
         let code = e.attr('data-code');
         let unicode = e.attr('data-unicode');
         let hex = e.attr('data-hex');
         let css = e.attr('data-css');
 
-        $('#show-entity').text(entity);
-        $('#show-code').text(code);
-        $('#show-unicode').text(unicode);
-        $('#show-hex').text(hex);
-        $('#show-css').text(css);
-        $('#show-show').text(e.text());
+        jQuery('#show-entity').text(entity);
+        jQuery('#show-code').text(code);
+        jQuery('#show-unicode').text(unicode);
+        jQuery('#show-hex').text(hex);
+        jQuery('#show-css').text(css);
+        jQuery('#show-show').text(e.text());
 
         console.log(entity, code, unicode, hex, css);
     });
   
-  $('#listchars a').click(function (e) {
+  jQuery('#listchars a').click(function (e) {
         setTimeout(function () {
             window.scrollTo({top: 0, behavior: 'auto'});
 
